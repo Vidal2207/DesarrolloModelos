15a238838c4c2fcc0700481a9174cca26da1c16d
Ramiro <rvidallumbreras@gmail.com>
2023-01-17 17:32:45 -0600

    Simulador con 4 tipos de patos

---
+++ Practices/SimuladorPatos/SimuladorPatos.java
@@ -0,0 +1,16 @@
+public class SimuladorPatos {
+    public static void main(String[] args) {
+        Duck mallarDuck,redHead, decoyDuck, rubberDuck;
+        mallarDuck = new MallardDuck();
+        mallarDuck.showDuck();
+
+        redHead = new ReadHeadDuck();
+        redHead.showDuck();
+
+        decoyDuck = new DecoyDuck();
+        decoyDuck.showDuck();
+
+        rubberDuck = new RubberDuck();
+        rubberDuck.showDuck();
+    }
+}
